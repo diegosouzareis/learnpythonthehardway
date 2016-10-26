2e8cb2c1e798d82b2f98634ba65ffce9ab32a39d
Diego Reis <diego.reis@nubank.com.br>
2016-10-26 20:27:33 -0200

    exercicio 36

---
+++ Exercicio 36/NUGame/game.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+from di import a, b, c, d, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
+
+print a
+print b
+visitor_name = raw_input("%s " % c)
+print d
+print f
+print g
+print h
+
+def
