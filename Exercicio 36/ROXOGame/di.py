7ba9354a8879b22363861bdda92a4181f14cc97e
Diego Reis <diego.reis@nubank.com.br>
2016-11-07 19:58:34 -0200

    Atualizacoes e melhoramento de codigo

--- Exercicio 36/ROXOGame/di.py
+++ Exercicio 36/ROXOGame/di.py
@@ -1,52 +1,23 @@
 # coding: utf-8
 # dialogos
-a = "Ola! tudo bem?"
-b = "Para começarmos digite o seu nome"
-c = "Qual o seu nome?"
-d = "Bem vindo ao ROXObank"
-e = "Bem vindo a sala dino!"
-f = "Hoje você poderar explorar algumas salas de reunião da aqui do ROXObank"
-g = "Vamos lá"
-h = """Em qual sala você quer entrar?
+
+decisoes = {'primeira': """Em qual sala você quer entrar?
 1 - Dino
 2 - WOW
-3 - Sauna"""
-i = "Bem vindo a sala "
-j1 = "Aqui nesta sala costumamos fazer alguns treinamentos e videos para o Youtube"
-k = "Aqui tambem temos um buraco negro"
-l = """1 - Gravar um video
-2 - entrar no buraco negro
-3 - Sair"""
-m = "Opsss hahaha, estamos sem ninguem para gravar você hoje"
-n = "Deseja mesmo entrar? Sim ou nao?"
-o = "Dino"
-os = "dino"
-p = "Wow"
-ps = "wow"
-q = "Sauna"
-qs = "sauna"
-r = """Aqui é uma sala de reunião bem comum, aqui voce pode:
+3 - Sauna""", 'segunda': """Aqui é uma sala de reunião bem comum, aqui voce pode:
 1 - Ligar a TV
 2 - Escrever no quadro
-3 - Sair"""
-s = "Escolha uma sala para entrar"
-t = "Sim"
-ti = "sim"
-u = "Nao"
-u2 = "nao"
-v = "Poxa cara nao seja deselegante"
-w = ":)"
-x = "Obrigado"
-y = "Sair"
-z = "Obrigado por vir visitar o ROXObank"
-z2 = "Essa sala não existe ou não esta disponivel para acesso!"
-z3 = "Parece que precisamos trocar as canetas depois voce escreve ;)"
-z4 = "Você ligou a TV, parece estar passando The Walking Dead"
-z5 = "VOCÊ NAO DEVERIA TER FEITO ISOOOOO, ESTAVA BRINCANDOOOOOOOOOOO, AGORA O ROXOBank ESTA EM PARADO NO TEMPOOOOO"
-z6 = "DEU MUITO RUIMMMMM"
-r2 = """Aqui é uma sala de reunião bem legal e bem grande tambem hahaha:
+3 - Sair""", 'terceira': """1 - Gravar um video
+2 - entrar no buraco negro
+3 - Sair""", 'quarta': """Aqui é uma sala de reunião bem legal e bem grande tambem hahaha:
 1 - Procurar alguma coisa
 2 - Tirar foto
-3 - Sair"""
-r3 = "O que voce esta fazendo, cuidado com o que procura MUHAAHAHA"
-r4 = "BOAAA, depois compatilhe essa foto :)"
+3 - Sair""" }
+
+salas = {'sala1': 'Dino', 'sala2': 'Sauna', 'sala3': 'Wow'}
+
+sim = "Sim"
+nao = "Nao"
+
+dialogos_basicos = {'1':'Bem vindo ao ROXObank', '2': 'Qual o seu nome?', '3': 'Para começarmos digite o seu nome', '4':'Ola! tudo bem?', '5': 'Hoje você poderar explorar algumas salas de reunião da aqui do ROXObank', '6':'Bem vindo a sala ', '7': 'Obrigado', '8':'Escolha uma sala para entrar', '9':'Vamos lá', '10': 'Sair', '11':'Obrigado por vir visitar o ROXObank', '12':':)'}
+dialogos = {'1': 'Opsss hahaha, estamos sem ninguem para gravar você hoje', '2': 'Deseja mesmo entrar? Sim ou nao?', '3': 'Aqui nesta sala costumamos fazer alguns treinamentos e videos para o Youtube', '4':'Aqui tambem temos um buraco negro', '5': 'Bem vindo a sala dino!', '6': 'Poxa cara nao seja deselegante', '7': 'Essa sala não existe ou não esta disponivel para acesso!', '8':'Parece que precisamos trocar as canetas depois voce escreve ;)', '9': 'Você ligou a TV, parece estar passando The Walking Dead', '10': 'VOCÊ NAO DEVERIA TER FEITO ISOOOOO, ESTAVA BRINCANDOOOOOOOOOOO, AGORA O ROXOBank ESTA EM PARADO NO TEMPOOOOO', '11': 'DEU MUITO RUIMMMMM', '12':'O que voce esta fazendo, cuidado com o que procura MUHAAHAHA', '13':'BOAAA, depois compatilhe essa foto :)'}
